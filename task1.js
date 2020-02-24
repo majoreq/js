b6bfd55bcbec8a9392631520972b33ace157e209
majoreq@gmail.com <majoreq@gmail.com>
2020-02-24 13:04:38 +0100

    show counter to end-user

--- task1.js
+++ task1.js
@@ -1,10 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
   var button = document.getElementsByClassName("btn btn-success");
-
   var counter = 0;
+
   function clicks(event){
     counter += 1;
-    console.log("click nr: " + counter );
+    button[0].addEventListener("click" , clicks);
+    var paragraph = document.getElementById("text");
+    paragraph.innerHTML = "click " + counter;
   }
 
   button[0].addEventListener("click" , clicks);
