cecc4f2d8d97ff11c32acad2bfe9e21baf1d62ff
majoreq@gmail.com <majoreq@gmail.com>
2020-02-24 15:44:57 +0100

    make 2 buttons with diffrence function, one to increment counter and one to add new button

--- task1.js
+++ task1.js
@@ -1,16 +1,21 @@
 document.addEventListener("DOMContentLoaded", function() {
-  var button = document.getElementsByClassName("btn btn-success");
+  var btn1 = document.getElementById("btn1");
+  var btn2 = document.getElementById("btn2");
   var counter = 0;
   var newButton = '<button type="button" class="btn btn-success">Success</button>'
+  var p1 = document.getElementById("p1");
+  var p2 = document.getElementById("p2");
 
   function clicks(event){
     counter += 1;
-    button[0].addEventListener("click" , clicks);
-    var paragraph = document.getElementById("text");
-
-    paragraph.innerHTML = "click " + counter;
-    paragraph.outerHTML += newButton;
+    p1.innerHTML = "click " + counter;
+    console.log(p1);
   }
 
-  button[0].addEventListener("click" , clicks);
+  function add_buton(event){
+    p2.innerHTML += newButton;
+  }
+
+  btn1.addEventListener("click" , clicks);
+  btn2.addEventListener("click" , add_buton);
 })
