91e2ce0d31828c73763200cbbba158a9945bb057
majoreq@gmail.com <majoreq@gmail.com>
2020-02-24 13:28:05 +0100

    clicking primiray button add new button

--- task1.js
+++ task1.js
@@ -1,12 +1,15 @@
 document.addEventListener("DOMContentLoaded", function() {
   var button = document.getElementsByClassName("btn btn-success");
   var counter = 0;
+  var newButton = '<button type="button" class="btn btn-success">Success</button>'
 
   function clicks(event){
     counter += 1;
     button[0].addEventListener("click" , clicks);
     var paragraph = document.getElementById("text");
+
     paragraph.innerHTML = "click " + counter;
+    paragraph.outerHTML += newButton;
   }
 
   button[0].addEventListener("click" , clicks);
