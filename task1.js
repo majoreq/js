e2491146ddbc18df325dc4c504ecdf21d42393e1
majoreq@gmail.com <majoreq@gmail.com>
2020-02-25 09:09:42 +0100

    added jQuery func to handle new button to increment counter

--- task1.js
+++ task1.js
@@ -2,20 +2,28 @@ document.addEventListener("DOMContentLoaded", function() {
   var btn1 = document.getElementById("btn1");
   var btn2 = document.getElementById("btn2");
   var counter = 0;
-  var newButton = '<button type="button" class="btn btn-success">Success</button>'
+  var newButton = '<button type="button" class="btn btn-success">NowyKliker</button>'
   var p1 = document.getElementById("p1");
   var p2 = document.getElementById("p2");
 
-  function clicks(event){
+
+  function clicks2(event){
     counter += 1;
     p1.innerHTML = "click " + counter;
-    console.log(p1);
   }
+
+  var clicks = $("#p2");
+  clicks.on('click','button', function(){
+    counter += 1;
+    p1.innerHTML = "click " + counter;
+  });
 
   function add_buton(event){
     p2.innerHTML += newButton;
   }
 
-  btn1.addEventListener("click" , clicks);
+  btn1.addEventListener("click" , clicks2);
   btn2.addEventListener("click" , add_buton);
+
+
 })
