7359f79a07d4510fce18808c2c088a92ecc2949c
majoreq@gmail.com <majoreq@gmail.com>
2020-02-25 14:46:37 +0100

    change html file to index, add hide/show button

--- task1.js
+++ task1.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
   var btn1 = document.getElementById("btn1");
   var btn2 = document.getElementById("btn2");
+  var btn3 = document.getElementById("btn3");
 
   var p1 = document.getElementById("p1");
   var p2 = document.getElementById("p2");
@@ -17,6 +18,18 @@ document.addEventListener("DOMContentLoaded", function() {
     p2.innerHTML += newButton;
   }
 
+  function hideButton(event) {
+    if (btn3.value == "show") {
+      document.getElementById("mainbody").style.display = "none";
+      btn3.value = "hide";
+    } else if (btn3.value == "hide") {
+      document.getElementById("mainbody").style.display = "";
+      btn3.value = "show";
+    }
+
+  }
+
+  $(btn3).on('click', hideButton);
   $(p2).on('click', 'button', clicks);
   btn1.addEventListener("click", clicks);
   btn2.addEventListener("click", addButton);
